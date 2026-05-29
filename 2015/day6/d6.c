8da6aa1410993f24a0256e1732e72f7f67b2e2ea
Stephen Slater <steve_slater@hotmail.com>
2026-05-29 07:03:06 -0400

    strtok

--- 2015/day6/d6.c
+++ 2015/day6/d6.c
@@ -4,16 +4,17 @@
 #include <string.h>
 typedef struct
 {
-    int upleft[2];
-    int lwrleft[2];
-    int upright[2];
-    int lwrright[2];
+    int rowstart;
+    int colstart;
+    int rowend;
+    int colend;
 } Coords;
 int main()
 {
     Coords coords[1000];
     char *token;
     FILE *fp;
+    char *options;
     fp = fopen("p1.txt", "rb");
     if (fp == NULL)
     {
@@ -38,10 +39,10 @@ int main()
     buffer[fileSize] = '\0'; // Null-terminate the string
     int lineindex = 0;
     bool turnon = true;
-    bool toggle = true;
+    bool toggle = false;
     for (int i = 0; i < bytesRead; i++)
     {
-        // Coords *current = malloc(sizeof(Coords));
+        Coords *current = malloc(sizeof(Coords));
 
         lineindex = 0;
         while (buffer[i] != '\n' && buffer[i] != '\r')
@@ -51,8 +52,28 @@ int main()
         }
         line[lineindex] = '\0'; // Null-terminate the line
         lineindex = 0;
-        printf("%s\n", line);
+        // printf("%s\n", line);
         token = strtok(line, ", ");
+        printf("%s\n", token);
+        while (token != NULL)
+        {
+            stricmp("toggle", token) == 0 ? toggle = true : false;
+
+            token = strtok(NULL, ", ");
+            printf("colstart: %s\n", token);
+            current->colstart = atoi(token);
+            token = strtok(NULL, ", ");
+            printf("colend: %s\n", token);
+            current->colend = atoi(token);
+            token = strtok(NULL, " ");
+            token = strtok(NULL, ", ");
+            printf("rowstart: %s\n", token);
+            current->rowstart = atoi(token);
+            token = strtok(NULL, ", ");
+            printf("rowend: %s\n", token);
+            current->rowend = atoi(token);
+        }
+        free(current);
     }
     // Do something with the buffer, e.g., print its contents
     printf("%s", buffer);

ea149fe9ee515ea4b72ae26dc687a03f2c8732e4
Stephen Slater <steve_slater@hotmail.com>
2026-06-06 05:34:31 -0400

    junk

--- 2015/day8/d8.c
+++ 2015/day8/d8.c
@@ -3,7 +3,7 @@
 #include <string.h>
 int main() {
     FILE *fp;
-    fp = fopen("s1.txt", "rb");
+    fp = fopen("test.txt", "rb"); // rb always
     if (fp == NULL) {
         printf("Error opening file\n");
         return 1;
@@ -21,6 +21,9 @@ int main() {
     fread(buffer, sizeof(char), file_size, fp);
     char line[100];
     int file_pos = 0;
+    long totalstrinsize = 0;
+    long totalinmemsize = 0;
+    long grandototalinmemsize = 0;
     while (file_pos < file_size) {
         int lineindex = 0;
 
@@ -29,24 +32,53 @@ int main() {
                buffer[file_pos] != '\r') {
             line[lineindex++] = buffer[file_pos++];
         }
-
+        totalinmemsize = 0;
         // Null-terminate the string immediately
         line[lineindex] = '\0';
         int len = strlen(line);
-        printf("%s is %d characters long\n", line, len);
+
+        totalinmemsize += len - 2;
+
+        //    printf("%s is %d characters long\n", line, len);
+        //    printf("Subtract 2 for quotes: %ld\n", totalinmemsize);
         for (int i = 0; i < len; i++) {
-            //            printf("%c ", line[i]);
             if (line[i] == '\\') {
-                printf("(backslash) at index %d\n", i - 1);
+
+                if (line[i + 1] == 'x') {
+                    totalinmemsize -= 3; // \xNN is 4 characters in the file but
+                    i += 3;              // represents 1 character in memory
+                    //                  printf("Subtract 3 for hex escape:
+                    //                  %d\n", totalinmemsize);
+                    continue;
+                }
+
+                if (line[i + 1] == '\\') {
+                    totalinmemsize -= 1; // \\ is 2 characters in the file but
+                                         // represents 1 character in memory
+                    i += 1;
+                    //                    printf("Subtract 1 for backslash:
+                    //                    %d\n", totalinmemsize);
+                    continue;
+                } else {
+                    totalinmemsize -= 1;
+                    i += 1;
+                    //  printf("Subtract 1 for regular escape: %d\n",
+                    //  totalinmemsize);
+                }
             }
         }
+        grandototalinmemsize += totalinmemsize;
+        totalstrinsize += len;
         // Skip ALL trailing newline/return characters (\n, \r, or \r\n)
         while (file_pos < file_size &&
                (buffer[file_pos] == '\n' || buffer[file_pos] == '\r')) {
             file_pos++;
         }
     }
-
+    long finalt = totalstrinsize - grandototalinmemsize;
+    printf("Total string size: %ld\n", totalstrinsize);
+    printf("Total in-memory size: %ld\n", grandototalinmemsize);
+    printf("Difference: %ld\n", finalt);
     free(buffer);
     fclose(fp);
     return 0;

dc8970c2b3af3a22671cad1753b080b53a5a7b95
Stephen Slater <steve_slater@hotmail.com>
2026-06-19 06:18:30 -0400

    p1

--- 2016/day1/d1.c
+++ 2016/day1/d1.c
@@ -1,3 +1,4 @@
+#include "../uthash.h"
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -5,13 +6,32 @@
 #include <string.h>
 #define ROWSTART 5000
 #define COLSTART 5000
+typedef struct {
+    char key[50];
+    int value;
+    UT_hash_handle hh; // makes this struct hashable
+} Entry;
 
+Entry *table = NULL;
+void printTable(Entry *table) {
+    int index = 0;
+    Entry *current, *tmp;
+    HASH_ITER(hh, table, current, tmp) {
+        printf("[%d] dkey: '%s' value: %d\n", index++, current->key,
+               current->value);
+    }
+    printf("---\n"); // set breakpoint on this line
+}
 int compute_distance(int row, int col) {
-    return ROWSTART - row + COLSTART - col;
+    return abs(ROWSTART - row + COLSTART - col);
 }
 int main() {
+    char loc[50];
+    char cloc[25];
+    char rloc[25];
+    Entry *found;
     bool notdone = true;
-    FILE *fp = fopen("s1.txt", "rb");
+    FILE *fp = fopen("p1.txt", "rb");
     fseek(fp, 0, SEEK_END);
     long filesize = ftell(fp);
     rewind(fp);
@@ -27,12 +47,24 @@ int main() {
     char prevdirection = 'N';
     int amount = 0;
     int linectr = 0;
+    int tottomove = 0;
+
+    itoa(col, cloc, 10);
+    itoa(row, rloc, 10);
+    strcpy(loc, cloc);
+    strcat(loc, rloc);
+    // char key[25];
+    // strcpy(key, loc);
+    Entry *entry = calloc(1, sizeof(Entry));
+    entry->value = 0;
+    strcpy(entry->key, loc);
+    HASH_ADD_STR(table, key, entry);
+
     while (fileindex < filesize) {
         linectr++;
         int lineindex = 0;
         if (buffer[fileindex] == '\r') {
-            row = 5000;
-            col = 5000;
+
             linectr++;
             prevdirection = 'N';
             direction = 'N';
@@ -59,43 +91,217 @@ int main() {
         case 'R':
             if (prevdirection == 'N') {
                 prevdirection = 'E';
-                col = col + amount;
+                tottomove = col + amount;
+
+                itoa(row, rloc, 10);
+
+                while (col++ < tottomove) {
+
+                    itoa(col, cloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+
+                col--;
             } else if (prevdirection == 'S') {
                 prevdirection = 'W';
-                col = col - amount;
+                tottomove = col - amount;
+                itoa(row, rloc, 10);
+                while (col-- > tottomove) {
+
+                    itoa(col, cloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+
+                col++;
             } else if (prevdirection == 'E') {
                 prevdirection = 'S';
-                row = row - amount;
+                tottomove = row - amount;
+                itoa(col, cloc, 10);
+
+                while (row-- > tottomove) {
+
+                    itoa(row, rloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                row++;
             } else if (prevdirection == 'W') {
                 prevdirection = 'N';
-                row = row + amount;
+                tottomove = row + amount;
+                itoa(col, cloc, 10);
+
+                while (row++ < tottomove) {
+
+                    itoa(row, rloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                row--;
             }
             break;
 
         case 'L':
             if (prevdirection == 'S') {
                 prevdirection = 'E';
-                col = col + amount;
+                tottomove = col + amount;
+                itoa(row, rloc, 10);
+                while (col++ < tottomove) {
+                    itoa(col, cloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                col--;
             } else if (prevdirection == 'N') {
                 prevdirection = 'W';
-                col = col - amount;
+                tottomove = col - amount;
+                itoa(row, rloc, 10);
+                while (col-- > tottomove) {
+
+                    itoa(col, cloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                col++;
             } else if (prevdirection == 'E') {
                 prevdirection = 'N';
-                row = row + amount;
+                tottomove = row + amount;
+                itoa(col, cloc, 10);
+
+                while (row++ < tottomove) {
+
+                    itoa(row, rloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                row--;
             } else if (prevdirection == 'W') {
                 prevdirection = 'S';
-                row = row - amount;
+                tottomove = row - amount;
+                itoa(col, cloc, 10);
+
+                while (row-- > tottomove) {
+                    itoa(row, rloc, 10);
+                    strcpy(loc, cloc);
+                    strcat(loc, rloc);
+                    HASH_FIND_STR(table, loc, found);
+                    if (found) {
+                        fileindex = filesize;
+                        int total = compute_distance(row, col);
+                        printf("\n%d", total);
+                        break;
+                    } else {
+                        Entry *entry = calloc(1, sizeof(Entry));
+                        char *key = strdup(loc);
+                        strcpy(entry->key, key);
+                        entry->value = 0;
+                        HASH_ADD_STR(table, key, entry);
+                    }
+                }
+                row++;
             }
             break;
 
         default:
             break;
         }
-        if (fileindex > 2 && linectr > 2) {
-            int distance = compute_distance(row, col);
-            printf("%d\n", abs(distance));
-        }
-    }
 
+        //   printTable(table);
+    }
+    if (fileindex > 2 && linectr > 2) {
+        int distance = compute_distance(row, col);
+    }
     return 0;
 }

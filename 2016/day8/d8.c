c946b12f9325ef9bd282934962d62c0da62f2f5b
Stephen Slater <steve_slater@hotmail.com>
2026-07-10 05:40:34 -0400

    d8 start

--- 2016/day8/d8.c
+++ 2016/day8/d8.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "uthash.h"
 
 typedef struct {
     int row;
     int col;
+    bool on;
     UT_hash_handle hh;
 } Point;
 
 Point *grid = NULL;
 
-void main() {
+int main() {
     int ROWMAX = 3;
     int COLMAX = 7;
     int row = 0;
     int col = 0;
     int grid[3][7];
+
     FILE *fp = fopen("s1.txt", "rb");
     long filesize = 0;
     fseek(fp, 0, SEEK_END);
@@ -26,8 +30,26 @@ void main() {
     if (bytesread != filesize) {
         perror("fread");
     }
+    fclose(fp);
     char *token;
     long fileindex = 0;
+    int lineindex = 0;
+    char temp[256];
+    int linectr = 0;
+
     while (fileindex < filesize) {
+        char *line = calloc(512, sizeof(char));
+        int lineindex = 0;
+        while (buffer[fileindex] == '\r' || buffer[fileindex] == '\n') {
+            fileindex++;
+        }
+        while (fileindex < filesize && buffer[fileindex] != '\r' &&
+               buffer[fileindex] != '\n') {
+            line[lineindex++] = buffer[fileindex++];
+        }
+        line[lineindex] = '\0';
+        printf("%s\n", line);
+        int len = strlen(line);
+        lineindex = 0;
     }
 }

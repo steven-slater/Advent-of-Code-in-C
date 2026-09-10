ba3f05a2e97eb39f02b900e9e6bcbb6f65ab0d19
Stephen Slater <steve_slater@hotmail.com>
2026-09-10 05:28:03 -0400

    p1 p2

--- 2017/day1/part2.c
+++ 2017/day1/part2.c
@@ -5,56 +5,51 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *fp = fopen("s1.txt", "rb");
+    FILE *fp = fopen("p1.txt", "rb");
     if (fp == NULL) {
         perror("fopen");
         return 1;
     }
+
     char *token;
-    char buffer[2050];
-    char left[1050];
-    char right[1050];
-    char key[4];
-    int weight;
+    // char buffer[2050];
+    char *buffer;
+    long filesize;
+    fseek(fp, 0, SEEK_END);
+    filesize = ftell(fp);
+
+    printf("%ld\n", filesize);
+    buffer = (calloc(filesize + 1, sizeof(char)));
+    rewind(fp);
+    long bytesRead = fread(buffer, sizeof(char), filesize, fp);
+
     char next[4];
     int subtot = 0;
     int total = 0;
-    while (fgets(buffer, 2037, fp) != '\0') {
-        buffer[strcspn(buffer, "\r\n")] = '\0';
-        int len = strlen(buffer);
-        strncpy(left, buffer, (len / 2) + 1);
-        left[(len / 2)] = '\0';
-        strncpy(right, buffer + (len / 2), ((len / 2) + 1));
-        int first = 0;
-        char firstc[2];
-        printf("%s\n", left);
-        for (int i = 0; i < len / 2; i++) {
-            // if (i == len - 1) {
-            //     // check last against first
-            //     strncpy(firstc, left + i, 1);
-            //     first = atoi(firstc);
-            //     char lastc[2];
-            //     strncpy(lastc, right + i, 1);
-            //     int last = atoi(lastc);
-            //     if (first == last) {
-            //         subtot += first;
-            //     }
-            //     continue;
-            // }
 
-            strncpy(firstc, left + i, 1);
-            first = atoi(firstc);
+    buffer[strcspn(buffer, "\r\n")] = '\0';
+    int len = strlen(buffer);
+    int step = len / 2;
+    printf("%s\n", buffer);
+    int first = 0;
+    char firstc[2];
+    // printf("%s\n%d\n", left, strlen(left));
+    for (int i = 0; i < len / 2; i++) {
 
-            char nextc[2];
-            strncpy(nextc, right + i, 1);
-            int next = atoi(nextc);
-            if (first == next) {
-                subtot += first + next;
-            }
+        strncpy(firstc, buffer + i, 1);
+        first = atoi(firstc);
+
+        char nextc[2];
+        strncpy(nextc, buffer + step + i, 1);
+        int next = atoi(nextc);
+        printf("%d ? %d %d\n", first, next, subtot);
+        if (first == next) {
+            subtot += first + next;
         }
-        total += subtot;
-        subtot = 0;
     }
+    total += subtot;
+    subtot = 0;
+
     printf("%d\n", total);
     return 0;
 }
